124a4e857d50c4bb7c1f0cefe0590410c3d85b6d
University of Tehran Computer Network Lab <40654897+utcnlab@users.noreply.github.com>
2018-12-29 14:56:21 +0330

    Update runDevInstaller.sh

--- scripts/runDevInstaller.sh
+++ scripts/runDevInstaller.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 sudo apt update && \
-sudo apt-get install libffi-dev python-dev build-essential
-sudo apt-get install mininet
+sudo apt-get install -y libffi-dev python-dev build-essential wireshark mininet
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install networkx ryu
 sudo pip install matplotlib
-sudo apt-get install python-tk
+sudo apt-get install -y python-tk
