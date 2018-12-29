52c2bbb443ed47188661c285e7fbe5b9f63e3833
University of Tehran Computer Network Lab <40654897+utcnlab@users.noreply.github.com>
2018-12-29 14:31:14 +0330

    Update runDevInstaller.sh

--- scripts/runDevInstaller.sh
+++ scripts/runDevInstaller.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt update
+sudo apt update && \
+sudo apt-get install libffi-dev python-dev build-essential
 sudo apt-get install mininet
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
